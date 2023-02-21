b9f5778596b763d3ed1a138f8330086a9f32b83b
EC2 Default User <ec2-user@ip-172-31-8-23.ap-northeast-1.compute.internal>
2023-02-21 13:43:09 +0000

    [Finish]4-24

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,9 +1,12 @@
 class PostImage < ApplicationRecord
-   has_one_attached :image
+    has_one_attached :image
     belongs_to :user
-    has_many :post_comments, dependent: :destroy
     has_many :favorites, dependent: :destroy
-
+    has_many :post_comments, dependent: :destroy
+# shop_nameが存在しているかを確認するバリデーション
+validates :shop_name,presence: true
+# imageが存在しているかを確認するバリデーション
+validates :image,presence: true
     def get_image
       unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
